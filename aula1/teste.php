23a52053e3c4e0598d3102382b06ad83cd9bbd34
DedeRuindadePura <familiamiranda1101@gmail.com>
2023-02-14 11:00:47 -0300

    Lolo Bundona

---
+++ aula1/teste.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<h1>Slap no flap na tcheca<br>
+    Splash, afogando meu pau na xereca<br>
+    Rocket poc, ploc, ploc, ploc<br>
+    Pula ni mim, perereca</h1>";
+?>
---
+++ aula1/teste2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+
+        <title></title>
+    </head>
+    <body>
+        <!-- aqui código html -->
+        <h1>Slap no flap na tcheca<br>
+        Splash, afogando meu pau na xereca<br>
+        Rocket poc, ploc, ploc, ploc<br>
+        Pula ni mim, perereca</h1>
+    </body>
+      <!-- aqui jaz olavo de carvalho -->
+    <?php
+    echo "<h3>Slap no flap na tcheca<br>
+    Splash, afogando meu pau na xereca<br>
+    Rocket poc, ploc, ploc, ploc<br>
+    Pula ni mim, perereca</h3> ";
+?>
+</html>
