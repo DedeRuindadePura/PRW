481cf48b54715a3a218f0aa31f59f334ea64776b
DedeRuindadePura <familiamiranda1101@gmail.com>
2023-04-25 10:17:17 -0300

    Muliro

--- Lista de formularios/8/tabuada.php
+++ Lista de formularios/8/tabuada.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
-$numero = $_POST['numero'];
-
-echo "<h2>Tabuada do $numero:</h2>";
-
-echo "<table>";
-for ($i=1; $i<=10; $i++) {
-	$resultado = $numero * $i;
-	echo "<tr><td>$numero x $i = </td><td>$resultado</td></tr>";
-}
-echo "</table>";
+    $number = $_POST['select'];
+    echo"<h3>Tabuada do $number:</h3>";
+    for($i=0; $i<=10; $i++){
+        $nuM= $number*$i;
+        echo"<h3> $number X $i = $nuM</h3>"; 
+    } 
+    
+    echo"<a href='exercicio8.html'><button>Voltar</button></a>";
+    
 ?>
-
-</body>
-</html>
