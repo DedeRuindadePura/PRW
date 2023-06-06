2cbc7cc48fe50b8226d660a356c12754485bd18d
DedeRuindadePura <familiamiranda1101@gmail.com>
2023-06-06 10:19:41 -0300

    Muliro

--- Lista5/altera_agenda_exe.php
+++ Lista5/altera_agenda_exe.php
@@ -1,26 +1,36 @@
 <?php  
     include('conexao.php');
-    $id_usuario = $_POST['id_usuario'];
+    $id_agenda = $_POST['id_agenda'];
     $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $fone = $_POST['fone'];
-    $senha = $_POST['senha'];
+    $apelido = $_POST['apelido'];
+    $endereco = $_POST['endereco'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+    $telefone = $_POST['telefone'];
+    $celular = $_POST['celular'];
+    $email= $_POST['email'];
 
     echo "<h1>Alteração de dados </h1>";
-    echo "<p>Usuario: $nome</p>";
-    $sql = "UPDATE usuario SET
-            nome_usuario='$nome',
-            email_usuario='$email',
-            fone_usuario='$fone',
-            senha='$senha' 
-            WHERE id_usuario=$id_usuario";
+    echo "<p>Agenda: $nome</p>";
+    $sql = "UPDATE agenda SET
+            nome='$nome',
+            apelido='$apelido',
+            endereco='$endereco',
+            bairro='$bairro',
+            cidade='$cidade',
+            estado='$estado',
+            telefone='$telefone',
+            celular='$celular',
+            email='$email'
+            WHERE id_agenda=$id_agenda";
         
-    echo $sql. "<br>";
+    //echo $sql. "<br>";
     $result = mysqli_query($con, $sql);
-    if($result)
+     if($result)
         echo "Dados alterados com sucesso!<br>";
     else
-        echo "Erro ao alterar dados: $msqli_erro($con)!";
+        echo "Erro ao alterar dados: ". mysqli_error($con)."!";
   
 ?>
 <a href="index.php">Voltar</a>
