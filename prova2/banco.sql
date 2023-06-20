2f5530d54ad722ccf9454ca67ee714d644ec2842
DedeRuindadePura <familiamiranda1101@gmail.com>
2023-06-20 11:00:22 -0300

    Muliro

--- prova2/banco.sql
+++ prova2/banco.sql
@@ -7,7 +7,7 @@ id int not null auto_increment,
 data date NOT NULL,
 tipo varchar(10) NOT NULL,
 valor decimal (10,2) NOT NULL,
-historico varchar(10) NOT NULL,
+historico varchar(150) NOT NULL,
 cheque varchar(3) NOT NULL,
 PRIMARY KEY (id)
 );
