12a49749cf8c685a54ff711f1b5b287b6eba5132
DedeRuindadePura <familiamiranda1101@gmail.com>
2023-08-08 11:13:59 -0300

    Muliro

--- projetoblog/Banco/banco_blog.sql
+++ projetoblog/Banco/banco_blog.sql
@@ -6,11 +6,11 @@ CREATE TABLE usuario (
     id int NOT NULL AUTO_INCREMENT,
     nome varchar(50) NOT NULL,
     email varchar(255) NOT NULL,
-    senha varchar(60) NOT NULL,
+    senha varchar (60) NOT NULL,
     data_criacao datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     ativo tinyint NOT NULL DEFAULT '0',
     adm tinyint NOT NULL DEFAULT '0',
-    PRIMARY KEY (id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE post (
@@ -22,5 +22,5 @@ CREATE TABLE post (
     data_postagem datetime NOT NULL,
     PRIMARY KEY(id),
     KEY fk_post_usuario_idx (usuario_id),
-    CONSTRAINT fk_post_usuario_idx FOREIGN KEY (usuario_id) REFERENCES usuario (id)
-);
+    CONSTRAINT fk_post_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);s
