2a27616d0d3d424ec5cef5257e64ee23a3091c2a
DedeRuindadePura <familiamiranda1101@gmail.com>
2023-10-17 07:39:56 -0300

    Muliro

--- projetoblog/tests/sql_test.php
+++ projetoblog/tests/sql_test.php
@@ -1,15 +1,14 @@
 <?php
-
     require_once '../core/sql.php';
 
     $id = 1;
-    $nome = 'dede';
-    $email = 'dederuindadepura@gmail.com';
-    $senha = '123456';
-    $dados = [  'nome'  => $nome,
-                'email' => $email,
-                'senha' => $senha];
-    
+    $nome = 'Mario';
+    $email = 'mukai@gmail.com';
+    $senha = 'mario123';
+    $dados = ['nome' => $nome
+            , 'email' => $email
+            , 'senha' => $senha];
+
     $entidade = 'usuario';
     $criterio = [['id', '=', $id]];
     $campos = ['id', 'nome', 'email'];
@@ -20,20 +19,15 @@
     print_r($criterio);
     echo '<br>';
 
-    // Teste geração INSERT
-    $instrucao = insert($entidade,$dados);
-    echo $instrucao.'<br>';
+    $instrucao = insert($entidade, $dados);
+    echo $instrucao . '<br>';
 
-    // Teste geração UPDATE
-    $instrucao = update($entidade, $dados,$criterio);
-    echo $instrucao.'<br>';
+    $instrucao = update($entidade, $dados, $criterio);
+    echo $instrucao . '<br>';
 
-    // Teste geração SELECT
     $instrucao = select($entidade, $campos, $criterio);
-    echo $instrucao.'<br>';
+    echo $instrucao . '<br>';
 
-    // Teste geração DELETE
     $instrucao = delete($entidade, $criterio);
-    echo $instrucao.'<br>';
-
+    echo $instrucao . '<br>';
 ?>
